9cc1a732d3998acbe400b60cd023ca30ef53446f
Ebenezer Isaac <ebnezr.isaac@gmail.com>
2021-07-30 09:58:28 +0530

    https

--- js/forecast.js
+++ js/forecast.js
@@ -1,14 +1,14 @@
 const key = 'NZYVTd4CzEcQbc0oDZckTylUjcQUjCEy';
 
 const getWeather = async (city_code)=>{
-    const url = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const url = "https://dataservice.accuweather.com/currentconditions/v1/";
     const query = `${city_code}?apikey=${key}`;
     const response = await fetch(url+query);
     const data = await response.json();
     return data[0];
 }
 const getCity = async (city) => {
-    const url = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const url = "https://dataservice.accuweather.com/locations/v1/cities/search";
     const query = `?apikey=${key}&q=${city}`;
     const response = await fetch(url + query);
     const data = await response.json();
